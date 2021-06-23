8f83d5128a4007bdde89e4d03ad231872b41471f
mercuryZ <15210820708@163.com>
2021-06-23 21:47:46 +0800

    greating and position of weather

--- js/weather.js
+++ js/weather.js
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
   // Here you can change your position
   // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-  let latitude = 37.774929;
-  let longitude = -122.419418;
+  let latitude = 39.904202;
+  let longitude = 116.407394;
 
   getWeather(latitude, longitude);
 }
